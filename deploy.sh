979608db882730c21e5b3d7f8a37beadc5873a7b
gitefu <out.grow.1028@gmail.com>
2021-10-20 12:12:37 +0900

    ファイルの修正

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+echo -e "\033[0;32mStart Build...\033[0m"
+hugo --cleanDestinationDir
+sleep 0.1
+
+echo -e "\033[0;32mCopy WEB data...\033[0m"
+cd ../site
+git rm -rf *
+cp -rf ../web_workspace/public/* .
+sleep 0.1
+
+echo -e "\033[0;32mNow Uploading...\033[0m"
+git add *
 git commit -m "$msg"
+git push
+sleep 0.1
 
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
+cd ../web_workspace
+echo -e "\033[0;32mDONE!\033[0m"
